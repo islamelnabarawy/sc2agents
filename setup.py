1c1440f290e851c6a8eabcb579c5f5693c148459
Islam Elnabarawy <islam.ossama@gmail.com>
2017-09-25 22:08:37 -0500

    Fix package information in setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(
     version='0.0.1',
     install_requires=['sc2gym', 'pysc2'],
     url='https://github.com/islamelnabarawy/sc2agents',
-    dependency_links=['https://github.com/islamelnabarawy/sc2gym/tarball/master#egg=package-0.0.1']
+    dependency_links=['https://github.com/islamelnabarawy/sc2gym/tarball/master#egg=sc2gym-0.0.1']
 )
