e974771cfb9a097b71ac141c16d2dd7daf24ea02
Islam Elnabarawy <islam.ossama@gmail.com>
2017-10-24 22:49:43 -0500

    Advance version number and add more requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ __author__ = 'Islam Elnabarawy'
 
 setup(
     name='sc2agents',
-    version='0.0.2',
-    install_requires=['sc2gym', 'pysc2', 'baselines'],
+    version='0.0.3',
+    install_requires=['sc2gym', 'pysc2', 'baselines', 'absl', 'gym'],
     url='https://github.com/islamelnabarawy/sc2agents',
     dependency_links=['https://github.com/islamelnabarawy/sc2gym/tarball/master#egg=sc2gym-0.0.2']
 )
